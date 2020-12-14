496da5966dafdfb6c0c68e7fa066189af08a8fab
MariaLorenz <maria.lorenz@iem.thm.de>
2020-12-14 15:37:10 +0100

    Activities und leere Layouts erstellt und zugewiesen

---
+++ app/src/main/java/com/example/cardsagainstyourliver/GamelistActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cardsagainstyourliver
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class GamelistActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gamelist)
+    }
+}
---
+++ app/src/main/java/com/example/cardsagainstyourliver/ManualActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cardsagainstyourliver
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ManualActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_manual)
+    }
+}
