f462e460ee08b02ce7909756c5fd3a6efabe7348
MariaLorenz <maria.lorenz@iem.thm.de>
2020-12-15 12:18:14 +0100

    Hauptmenü Navigation zu anderen Activities & funktionierender wechsel zum Spezialmodus(Layout)

--- app/src/main/java/com/example/cardsagainstyourliver/MainActivity.kt
+++ app/src/main/java/com/example/cardsagainstyourliver/MainActivity.kt
@@ -1,12 +1,50 @@
 package com.example.cardsagainstyourliver
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Activitywechsel zu GameListActivity via Button
+        val buttonPlay: Button = findViewById(R.id.play)
+        buttonPlay.setOnClickListener {
+            val testGamelist = Intent(this, GamelistActivity::class.java)
+            startActivity(testGamelist)
+        }
+
+        val buttonSpecialmode: Button = findViewById(R.id.specialmode)
+        buttonSpecialmode.setOnClickListener {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        }
+
+        //Activitywechsel zu ProfileActivity via Button
+        val buttonProfile: Button = findViewById(R.id.profile)
+        buttonProfile.setOnClickListener {
+            val testProfile = Intent(this, ProfileActivity::class.java)
+            startActivity(testProfile)
+        }
+        //Activitywechsel zu ManualActivity via Button
+        val buttonManual: Button = findViewById(R.id.manual)
+        buttonManual.setOnClickListener {
+            val testManual = Intent(this, ManualActivity::class.java)
+            startActivity(testManual)
+        }
+        //Activitywechsel zu SettingsActivity via Button
+        val buttonSettings: Button = findViewById(R.id.settings)
+        buttonSettings.setOnClickListener {
+            val testSettings = Intent(this, SettingsActivity::class.java)
+            startActivity(testSettings)
+        }
     }
+
 }
+
+
+
--- app/src/main/java/com/example/cardsagainstyourliver/SettingsActivity.kt
+++ app/src/main/java/com/example/cardsagainstyourliver/SettingsActivity.kt
@@ -1,6 +1,8 @@
 package com.example.cardsagainstyourliver
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
@@ -8,5 +10,12 @@ class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        //Activitywechsel Zurück via Button
+        val buttonBack: ImageButton = findViewById(R.id.back_button)
+        buttonBack.setOnClickListener {
+            val testBack = Intent(this, MainActivity::class.java)
+            startActivity(testBack)
+        }
     }
 }
