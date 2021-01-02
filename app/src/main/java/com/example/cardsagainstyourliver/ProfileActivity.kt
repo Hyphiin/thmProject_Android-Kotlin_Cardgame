c57d56d1f6a04bd3d0fe9fa849ea85482241b556
Lasse <lasse.kreuter@iem.thm.de>
2021-01-02 22:46:04 +0100

    Listview mit Arrayadapter
    Ermöglicht navigation in bestimmte DB Einträge

--- app/src/main/java/com/example/cardsagainstyourliver/ProfileActivity.kt
+++ app/src/main/java/com/example/cardsagainstyourliver/ProfileActivity.kt
@@ -4,6 +4,10 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.profile_acivity_new.*
 
@@ -13,11 +17,33 @@ class ProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.profile_acivity_new)//eigentlich: activity_profile
 
-        val context = this
+        val context = this//hier werden die namen aus der DB geholt und angezeigt kann auch auf beliebige andere View elemente angewendet werden...
         var db =DBHandler(context)
+        var names: MutableList<String> = ArrayList()
         var data=db.readData()
         for(i in 0..(data.size-1)){
-            tv_Result.append(data.get(i).playerName)
+            names.add(data.get(i).playerName)
+        }
+
+        val adapter = ArrayAdapter(this,
+            R.layout.name_list_item, names) //namelistitem für design einzelner einträge
+
+        val nameList: ListView = findViewById(R.id.name_list)
+        nameList.setAdapter(adapter)
+
+        nameList.onItemClickListener = object : AdapterView.OnItemClickListener {
+
+            override fun onItemClick(parent: AdapterView<*>, view: View,
+                                     position: Int, id: Long) {
+
+
+                val itemValue = nameList.getItemAtPosition(position) as String
+
+                // Toast the values
+                Toast.makeText(applicationContext,
+                    "Position :$position\nItem Value : $itemValue", Toast.LENGTH_LONG)
+                    .show()
+            }
         }
     }
 
