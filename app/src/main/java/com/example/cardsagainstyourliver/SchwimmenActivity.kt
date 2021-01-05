ba5c4f797e2bcc073a33489c19f332958ba66410
MariaLorenz <maria.lorenz@iem.thm.de>
2021-01-05 20:26:08 +0100

    Drag And Drop Grundfunktion

--- app/src/main/java/com/example/cardsagainstyourliver/SchwimmenActivity.kt
+++ app/src/main/java/com/example/cardsagainstyourliver/SchwimmenActivity.kt
@@ -8,9 +8,12 @@ import android.os.Bundle
 import android.view.DragEvent
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_schwimmen.*
 
 class SchwimmenActivity : AppCompatActivity() {
 
@@ -20,20 +23,29 @@ class SchwimmenActivity : AppCompatActivity() {
         setContentView(R.layout.activity_schwimmen)
 
 
-        val dragView: View = findViewById(R.id.player_card_01)!!
-        val dragView2: View = findViewById(R.id.player_card_02)!!
-        val dragView3: View = findViewById(R.id.player_card_03)!!
-        val llTopView: View = findViewById(R.id.llTop)!!
-        val llBottomView: View = findViewById(R.id.llBottom)!!
+        val dragView1: ImageView = findViewById(R.id.player_card_01)!!
+        val dragView2: ImageView = findViewById(R.id.player_card_02)!!
+        val dragView3: ImageView = findViewById(R.id.player_card_03)!!
 
-        llTopView.setOnDragListener(dragListener)
-        llBottomView.setOnDragListener(dragListener)
+        val dragView01: ImageView = findViewById(R.id.table_card_01)!!
+        val dragView02: ImageView = findViewById(R.id.table_card_02)!!
+        val dragView03: ImageView = findViewById(R.id.table_card_03)!!
+
+
+
+
+        table_left.setOnDragListener(dragListener)
+        table_middle.setOnDragListener(dragListener)
+        table_right.setOnDragListener(dragListener)
+        //tableCard3.setOnDragListener(dragListener)
+
+        player_left.setOnDragListener(dragListener)
+        player_middle.setOnDragListener(dragListener)
+        player_right.setOnDragListener(dragListener)
 
         // first drag and drop card
-        dragView.setOnLongClickListener {
-
-
-            val clipText = "Test"
+        dragView1.setOnLongClickListener {
+            val clipText = "Player Card Left"
             val item = ClipData.Item(clipText)
             val mimeTypes = arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN)
             val data = ClipData(clipText, mimeTypes, item)
@@ -48,9 +60,7 @@ class SchwimmenActivity : AppCompatActivity() {
 
         //new drag and drop card
         dragView2.setOnLongClickListener {
-
-
-            val clipText = "Test"
+            val clipText = "Player Card Middle"
             val item2 = ClipData.Item(clipText)
             val mimeTypes = arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN)
             val data = ClipData(clipText, mimeTypes, item2)
@@ -65,9 +75,7 @@ class SchwimmenActivity : AppCompatActivity() {
 
         //new drag and drop card
         dragView3.setOnLongClickListener {
-
-
-            val clipText = "Test"
+            val clipText = "Player Card Right"
             val item3 = ClipData.Item(clipText)
             val mimeTypes = arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN)
             val data = ClipData(clipText, mimeTypes, item3)
@@ -80,6 +88,47 @@ class SchwimmenActivity : AppCompatActivity() {
 
         }
 
+
+        dragView01.setOnLongClickListener {
+            val clipText = "Table Card Left"
+            val item01 = ClipData.Item(clipText)
+            val mimeTypes = arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN)
+            val data = ClipData(clipText, mimeTypes, item01)
+
+            val dragShadowBuilder = View.DragShadowBuilder(it)
+            it.startDragAndDrop(data, dragShadowBuilder, it, 0)
+
+            it.visibility = View.INVISIBLE
+            true
+
+        }
+        dragView02.setOnLongClickListener {
+            val clipText = "Table Card Middle"
+            val item02 = ClipData.Item(clipText)
+            val mimeTypes = arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN)
+            val data = ClipData(clipText, mimeTypes, item02)
+
+            val dragShadowBuilder = View.DragShadowBuilder(it)
+            it.startDragAndDrop(data, dragShadowBuilder, it, 0)
+
+            it.visibility = View.INVISIBLE
+            true
+
+        }
+
+        dragView03.setOnLongClickListener {
+            val clipText = "Table Card Right"
+            val item03 = ClipData.Item(clipText)
+            val mimeTypes = arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN)
+            val data = ClipData(clipText, mimeTypes, item03)
+
+            val dragShadowBuilder = View.DragShadowBuilder(it)
+            it.startDragAndDrop(data, dragShadowBuilder, it, 0)
+
+            it.visibility = View.INVISIBLE
+            true
+
+        }
     }
 
 
@@ -110,7 +159,11 @@ class SchwimmenActivity : AppCompatActivity() {
             DragEvent.ACTION_DROP -> {
                 val item = event.clipData.getItemAt(0)
                 val dragData = item.text
-                // Toast.makeText(this,dragData, Toast.LENGTH_SHORT).show()
+
+                Toast.makeText(this, dragData, Toast.LENGTH_SHORT).show()
+                //var test : ImageView = findViewById(R.id.table_card_01)
+                //test.setImageResource(R.drawable.card_caro_10)
+
 
                 view.invalidate()
 
@@ -119,6 +172,7 @@ class SchwimmenActivity : AppCompatActivity() {
                 owner.removeView(v)
                 val destination = view as LinearLayout
                 destination.addView(v)
+
                 v.visibility = View.VISIBLE
                 true
             }
