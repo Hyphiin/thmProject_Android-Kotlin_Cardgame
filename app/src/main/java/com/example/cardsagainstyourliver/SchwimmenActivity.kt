adb05101a00bf3b40d9d0fcefeec61c70809ae32
MariaLorenz <maria.lorenz@iem.thm.de>
2021-01-05 20:17:13 +0100

    radiogroups wechsel

--- app/src/main/java/com/example/cardsagainstyourliver/SchwimmenActivity.kt
+++ app/src/main/java/com/example/cardsagainstyourliver/SchwimmenActivity.kt
@@ -1,24 +1,87 @@
 package com.example.cardsagainstyourliver
 
+import android.content.ClipData
+import android.content.ClipDescription
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.view.DragEvent
 import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 
 class SchwimmenActivity : AppCompatActivity() {
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_spielerauswahl)
-
-
-
-    }
-
-
-    fun onClickStartGame(view: View) {
         setContentView(R.layout.activity_schwimmen)
+
+
+        val dragView: View = findViewById(R.id.player_card_01)!!
+        val dragView2: View = findViewById(R.id.player_card_02)!!
+        val dragView3: View = findViewById(R.id.player_card_03)!!
+        val llTopView: View = findViewById(R.id.llTop)!!
+        val llBottomView: View = findViewById(R.id.llBottom)!!
+
+        llTopView.setOnDragListener(dragListener)
+        llBottomView.setOnDragListener(dragListener)
+
+        // first drag and drop card
+        dragView.setOnLongClickListener {
+
+
+            val clipText = "Test"
+            val item = ClipData.Item(clipText)
+            val mimeTypes = arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN)
+            val data = ClipData(clipText, mimeTypes, item)
+
+            val dragShadowBuilder = View.DragShadowBuilder(it)
+            it.startDragAndDrop(data, dragShadowBuilder, it, 0)
+
+            it.visibility = View.INVISIBLE
+            true
+
+        }
+
+        //new drag and drop card
+        dragView2.setOnLongClickListener {
+
+
+            val clipText = "Test"
+            val item2 = ClipData.Item(clipText)
+            val mimeTypes = arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN)
+            val data = ClipData(clipText, mimeTypes, item2)
+
+            val dragShadowBuilder = View.DragShadowBuilder(it)
+            it.startDragAndDrop(data, dragShadowBuilder, it, 0)
+
+            it.visibility = View.INVISIBLE
+            true
+
+        }
+
+        //new drag and drop card
+        dragView3.setOnLongClickListener {
+
+
+            val clipText = "Test"
+            val item3 = ClipData.Item(clipText)
+            val mimeTypes = arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN)
+            val data = ClipData(clipText, mimeTypes, item3)
+
+            val dragShadowBuilder = View.DragShadowBuilder(it)
+            it.startDragAndDrop(data, dragShadowBuilder, it, 0)
+
+            it.visibility = View.INVISIBLE
+            true
+
+        }
+
     }
+
 
     fun onClickPauseMenuButton(view: View) {
         val PauseMenuButton = Intent(this, PauseMenuActivity::class.java)
@@ -28,6 +91,43 @@ class SchwimmenActivity : AppCompatActivity() {
     fun onClickBackToMenuButton(view: View) {
         val BackToMenuButton = Intent(this, MainActivity::class.java)
         startActivity(BackToMenuButton)
+    }
+
+    val dragListener = View.OnDragListener { view, event ->
+        when (event.action) {
+            DragEvent.ACTION_DRAG_STARTED -> {
+                event.clipDescription.hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)
+            }
+            DragEvent.ACTION_DRAG_ENTERED -> {
+                view.invalidate()
+                true
+            }
+            DragEvent.ACTION_DRAG_LOCATION -> true
+            DragEvent.ACTION_DRAG_EXITED -> {
+                view.invalidate()
+                true
+            }
+            DragEvent.ACTION_DROP -> {
+                val item = event.clipData.getItemAt(0)
+                val dragData = item.text
+                // Toast.makeText(this,dragData, Toast.LENGTH_SHORT).show()
+
+                view.invalidate()
+
+                val v = event.localState as View
+                val owner = v.parent as ViewGroup
+                owner.removeView(v)
+                val destination = view as LinearLayout
+                destination.addView(v)
+                v.visibility = View.VISIBLE
+                true
+            }
+            DragEvent.ACTION_DRAG_ENDED -> {
+                view.invalidate()
+                true
+            }
+            else -> true
+        }
     }
 
 
