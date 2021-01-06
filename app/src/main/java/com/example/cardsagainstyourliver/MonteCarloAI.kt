f6981b638cdbcf461ef93867c16cad27ab6cecdd
Felix Wegener <felix.wegener@iem.thm.de>
2021-01-06 16:34:06 +0100

    Klassen, Interfaces wurde mit Code befüllt.
    Diesen Code gilt es zu erweitern!

--- app/src/main/java/com/example/cardsagainstyourliver/MonteCarloAI.kt
+++
@@ -1,4 +0,0 @@
-package com.example.cardsagainstyourliver
-
-class MonteCarloAI(var game, val UCB1ExploreParam: Int = 2) {
-}
--- app/src/main/java/com/example/cardsagainstyourliver/MonteCarloNode.kt
+++
@@ -1,4 +0,0 @@
-package com.example.cardsagainstyourliver
-
-class MonteCarloNode {
-}
