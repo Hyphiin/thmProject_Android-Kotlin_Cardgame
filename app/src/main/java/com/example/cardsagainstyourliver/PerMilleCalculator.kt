90c2bd1d56f6f98766bcb3c8b2f5e5955596ee07
Felix Wegener <felix.wegener@iem.thm.de>
2021-01-16 18:23:00 +0100

    Promillerechner 2.0
    Erweiterung der Funktionalität.

--- app/src/main/java/com/example/cardsagainstyourliver/PerMilleCalculator.kt
+++ app/src/main/java/com/example/cardsagainstyourliver/PerMilleCalculator.kt
@@ -2,21 +2,39 @@ package com.example.cardsagainstyourliver
 
 class PerMilleCalculator {
 
-    fun permille(mPerson: Double, gender: String, mAlkohol: Double, vDrink: Double): Double{
+    fun permille(mPerson: Double, gender: String, bierml: Double, weinml: Double, wodkaml: Double, Dauer: Int): Double{
 
         var r: Double
+        var BAK: Double
+
+        var konsuming: Double
+        var konsumbier: Double
+        var konsumwein: Double
+        var konsumwodka: Double
+
+        konsumbier = bierml*5.0 / 100*0.8;
+        konsumwein = weinml*14.0 / 100*0.8;
+        konsumwodka = wodkaml*40.0 / 100*0.8;
+
+        konsuming = konsumbier + konsumwein + konsumwodka
 
         if(gender == "m"){
             r = 0.7
+            BAK = konsuming / mPerson*0.7
         }else{
             r = 0.6
+            BAK = konsuming / mPerson*0.6
         }
 
-        var alkM : Double = 10.0 * 1 * mAlkohol * 0.8
+        for (i in 0 until Dauer) {
+            BAK = BAK * 0.85
+        }
 
-        var p : Double = (alkM*(mPerson*r))
+        BAK = Math.round(BAK * 100.0) / 100.0
 
-        return p
+        return BAK
+
+
 
     }
 
