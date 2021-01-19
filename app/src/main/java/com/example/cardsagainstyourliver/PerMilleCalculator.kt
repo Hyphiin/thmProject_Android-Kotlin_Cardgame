1cb85ff0bea2522f21f1269d0c1f29a9b1a955c3
Felix Wegener <felix.wegener@iem.thm.de>
2021-01-19 20:34:57 +0100

    Promillerechner geupdated.
    Version 3.0

--- app/src/main/java/com/example/cardsagainstyourliver/PerMilleCalculator.kt
+++ app/src/main/java/com/example/cardsagainstyourliver/PerMilleCalculator.kt
@@ -2,38 +2,24 @@ package com.example.cardsagainstyourliver
 
 class PerMilleCalculator {
 
-    fun permille(mPerson: Double, gender: String, bierml: Double, weinml: Double, wodkaml: Double, whiskeyml: Double, ginml: Double, sektml: Double, tequilaml: Double, rumml: Double,  Dauer: Int): Double{
+    fun permille(mPerson: Double, gender: String, percentage: Double, ml: Double, Dauer: Int): Double{
 
         var r: Double
-        var BAK: Double
+        var BAK: Double = 0.0
 
         var konsuming: Double
-        var konsumbier: Double
-        var konsumwein: Double
-        var konsumwodka: Double
-        var konsumwhiskey: Double
-        var konsumgin: Double
-        var konsumsekt: Double
-        var konsumtequila: Double
-        var konsumrum: Double
 
-        konsumbier = bierml*5.0 / 100*0.8
-        konsumwein = weinml*14.0 / 100*0.8
-        konsumwodka = wodkaml*40.0 / 100*0.8
-        konsumwhiskey = whiskeyml*63.4 / 100*0.8
-        konsumgin = ginml*40.0 / 100*0.8
-        konsumsekt = sektml*12.8 / 100*0.8
-        konsumtequila = tequilaml*38.0 / 100*0.8
-        konsumrum = rumml*37.5 / 100*0.8
+        konsuming = (ml*percentage)/(100.0*0.8)
 
-        konsuming = konsumbier + konsumwein + konsumwodka + konsumwhiskey + konsumgin + konsumsekt + konsumtequila + konsumrum
 
-        if(gender == "m"){
-            r = 0.7
-            BAK = konsuming / mPerson*0.7
-        }else{
-            r = 0.6
-            BAK = konsuming / mPerson*0.6
+        when(gender){
+            "m" ->{
+                r = 0.7
+                BAK = konsuming / mPerson*r}
+            "w" ->{
+                r = 0.6
+                BAK = konsuming / mPerson*r
+            }
         }
 
         for (i in 0 until Dauer) {
