08a027369e6f23574a481f9ae8c6abff32bdc5ee
MariaLorenz <maria.lorenz@iem.thm.de>
2021-01-22 12:22:16 +0100

    schwimmen Layout bug fixed

--- app/src/main/java/com/example/cardsagainstyourliver/PopUpSpielendeActivity.kt
+++ app/src/main/java/com/example/cardsagainstyourliver/PopUpSpielendeActivity.kt
@@ -5,14 +5,10 @@ import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.DisplayMetrics
-import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatDelegate
-import androidx.core.view.isVisible
-import kotlinx.android.synthetic.main.profile_acivity_new.*
 
 class PopUpSpielendeActivity : Activity() {
 
@@ -58,7 +54,6 @@ class PopUpSpielendeActivity : Activity() {
         } else {
             promille_btn.setVisibility(View.INVISIBLE)
         }
-
         next_round_btn.setOnClickListener {
             onClickGameStartButton(p1ID, p2ID, p1Pos, p2Pos, "Schwimmen")
         }
